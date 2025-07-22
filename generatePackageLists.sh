92449a0b91150db459fb47fd85223d155936ed4d
crypticsignals <cryptsig@proton.me>
2025-07-22 10:53:05 -0400

    Updated generatePackageLists logic and gitignore.

--- generatePackageLists.sh
+++ generatePackageLists.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-ORIG_DIR=$(pwd)
-cd ~
+HOST_NAME=$(hostname)
+DATA_DIR=$(readlink -f data/)
 
 # PACKAGES
-touch .packagelist.txt
-echo "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" > ~/Linux/Installed\ Packages.txt
-dnf repoquery --userinstalled --qf "%{name}\n " > ~/Linux/Installed\ Packages.txt
-cut -f 4 -d' ' .packagelist.txt >> ~/Linux/my-fedora-postinstall/Installed\ Packages.txt
+dnf repoquery --userinstalled --qf "%{name}\n" > $DATA_DIR/$HOST_NAME-installedPackages.txt
+echo "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" >> $DATA_DIR/$HOST_NAME-installedPackages.txt
+echo "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" >> $DATA_DIR/$HOST_NAME-installedPackages.txt
+
 
 # FLATPAKS
-flatpak list --columns=application > ~/Linux/my-fedora-postinstall/Installed\ Flatpaks.txt
-
-# Remove temporary file and cd back to where you were
-rm .packagelist.txt
-cd $ORIG_DIR
+flatpak list --columns=application > "$DATA_DIR/$HOST_NAME-installedFlatpaks.txt"
